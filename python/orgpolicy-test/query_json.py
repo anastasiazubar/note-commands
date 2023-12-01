697c05a19e71b1606528c4240579c202bc7cd1fa
Anastasiia Zubar <anazubar@WKMZTBDF50E0.global.publicisgroupe.net>
2023-12-01 15:51:29 -0600

    wip

--- python/orgpolicy-test/query_json.py
+++ python/orgpolicy-test/query_json.py
@@ -16,3 +16,31 @@ if description_matches:
         print(f"Description {index}: {description_value}")
 else:
     print("No description fields found.")
+
+
+
+
+##########
+
+
+import re
+
+error_message = "400 The request has violated one or more Org Policies. Please refer to the respective violations for more information. [violations {\n"\
+                "type: \"constraints/cloudfunctions.allowed/pcConnectorEgressSettings\"\n"\
+                "subject: \"orgpolicy:projects/pri-iam-denytest\"\n"\
+                "description: *Constraint constraints/cloudfunctions. allowed/pcConnectorEgresssettings violated for projects/prj-iam-denytest attempting CreateFunctionActionV with vpc_connector _egress _detting\n"\
+                "5 set to VPC CONNECTOR EGRESS SETTINGS UNSPECIFIED. See https://cloud.google.com/resource-manager/docs/organization-policy/org-policy-constraints for more information.\"\n"\
+                "description: *Another description field\n"\
+                "}] ** message-> The request has violated one or more Org Policies. Please refer to the respective violations for more information."
+
+description_matches = re.findall(r"description: \"(.*?)\"", error_message)
+description_list = []
+
+for index, description_value in enumerate(description_matches, start=1):
+    variable_name = f"description_{index}"
+    description_list.append({variable_name: description_value})
+
+# Print the created variables
+for item in description_list:
+    for key, value in item.items():
+        print(f"{key}: {value}")
