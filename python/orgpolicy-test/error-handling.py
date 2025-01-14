8177c08b8a4deef871682032c73c9dcde44a3063
Anastasiia Zubar <anazubar@WKMZTBDF50E0.global.publicisgroupe.net>
2025-01-14 14:28:29 -0600

    wip

--- python/orgpolicy-test/error-handling.py
+++ python/orgpolicy-test/error-handling.py
@@ -7,3 +7,15 @@ try:
     
     # Raise exception with the specific message
     raise Exception(f"Exception happened: {error_message}")
+
+
+
+
+    error_reason = (
+        error_details.get("error", {})
+        .get("details", [{}])[0]
+        .get("reason", "No reason found")
+    )
+
+    # Raise an exception with the combined message and reason
+    raise Exception(f"Exception happened: {error_message}, Reason: {error_reason}")
