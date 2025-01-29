b3e77495df7e4aea19a7404513701ca5d931deeb
Anastasiia Zubar <anazubar@WKMZTBDF50E0.global.publicisgroupe.net>
2025-01-29 13:34:34 -0600

    wip

--- python/orgpolicy-test/lb.py
+++ python/orgpolicy-test/lb.py
@@ -31,3 +31,40 @@ def create_https_lb_with_tls(project_id):
 
 # Usage
 create_https_lb_with_tls("your-project-id")
+
+
+
+
+Correct IAM Permissions Required
+You need these specific permissions to create and configure an HTTPS Load Balancer:
+
+1. Forwarding Rule Permissions
+compute.globalForwardingRules.create
+compute.globalForwardingRules.get
+compute.globalForwardingRules.list
+compute.globalForwardingRules.use
+2. Target HTTPS Proxy Permissions
+compute.targetHttpsProxies.create
+compute.targetHttpsProxies.get
+compute.targetHttpsProxies.list
+compute.targetHttpsProxies.use
+3. SSL Policy Permissions (To Specify TLS Version)
+compute.sslPolicies.create
+compute.sslPolicies.get
+compute.sslPolicies.list
+compute.sslPolicies.use
+4. URL Map Permissions
+compute.urlMaps.create
+compute.urlMaps.get
+compute.urlMaps.list
+compute.urlMaps.use
+5. SSL Certificate Permissions
+compute.sslCertificates.create
+compute.sslCertificates.get
+compute.sslCertificates.list
+compute.sslCertificates.use
+6. Backend Service Permissions
+compute.backendServices.create
+compute.backendServices.get
+compute.backendServices.list
+compute.backendServices.use
